8a8c1ab4553a86714f0bf5fafbe0547ebf6ef583
soethan98 <soethan762@gmail.com>
2024-01-14 03:57:39 +0800

    Login validation finished

--- App.tsx
+++ App.tsx
@@ -4,115 +4,125 @@
  *
  * @format
  */
+import { Button, Image, SafeAreaView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import * as Yup from 'yup'
+import { Formik } from 'formik'
+import Toast from 'react-native-simple-toast';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const loginValidationSchema = Yup.object().shape({
+  email: Yup.string().email("Please enter valid emial").required("Email is required"),
+  password: Yup.string().min(8, 'Password must be atleast 8 characters')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+      'Password must include lowercase, uppercase, and numbers'
+    ).required('Password is required')
+})
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+    <SafeAreaView style={styles.appContainer}>
+      <View style={styles.viewContainer}>
+        <View style={styles.imageContainer}>
+          <Image source={require('./images/logo.png')} style={styles.appLogo} resizeMode='contain' />
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
-      </ScrollView>
+        <View style={styles.formContainer}>
+          <Formik initialValues={{
+            email: '',
+            password: ''
+          }} onSubmit={values => { Toast.show('Success', 3) }}
+            validationSchema={loginValidationSchema}>
+            {({
+              values,
+              errors,
+              touched,
+              isValid,
+              handleChange,
+              handleSubmit
+            }) => (
+              <>
+                <View style={styles.fieldContainer}>
+                  <TextInput
+                    value={values.email} style={styles.emailStyle} placeholder='Email' onChangeText={handleChange('email')} />
+                  {touched.email && errors.email && (
+                    <Text style={styles.errorText}>
+                      {errors.email}
+                    </Text>
+                  )}
+                </View>
+                <View style={styles.fieldContainer}>
+                  <TextInput value={values.password} style={styles.emailStyle} placeholder='password' onChangeText={handleChange('password')} />
+                  {touched.password && errors.password && (
+                    <Text style={styles.errorText} >
+                      {errors.password}
+                    </Text>
+                  )}
+
+                </View>
+                <TouchableOpacity disabled={!isValid} onPress={() => handleSubmit()} style={styles.loginButton}>
+
+                  <Text style={styles.loginText}>{'Login'.toUpperCase()}</Text>
+                </TouchableOpacity>
+              </>
+            )}
+
+          </Formik>
+
+        </View>
+
+      </View>
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  appContainer: {
+    flex: 1
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  viewContainer: {
+    flex: 1,
+    marginHorizontal: 16,
+    marginVertical: 24,
+    justifyContent: 'space-between'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  formContainer: {
+    flex: 2
   },
-  highlight: {
-    fontWeight: '700',
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center'
   },
-});
+  appLogo: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center'
 
-export default App;
+  },
+  fieldContainer: {
+    marginBottom: 24
+  },
+  emailStyle: {
+    borderColor: '#16213e',
+    borderWidth: 1
+  }
+  ,
+  loginButton: {
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    backgroundColor: '#2596be',
+
+  },
+  loginText: {
+    color: '#ffffff'
+  },
+  errorText: {
+    fontSize: 10,
+    color: '#ff5733'
+  }
+
+
+})
